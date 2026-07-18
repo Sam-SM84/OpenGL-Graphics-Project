b06c1a3017df6820ba5a0f0814f3ef58c3bd82a5
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-07-18 12:20:22 +0330

    Update Gridbox.cpp

--- Src/Gridbox.cpp
+++ Src/Gridbox.cpp
@@ -1,28 +1,36 @@
 #include "Gridbox.h"
 
-Gridbox::Gridbox() 
-{ 
-    itemID = 0;
-    count = 0; 
-    VBO = 0;
+unsigned int Gridbox::VAO = 0;
+unsigned int Gridbox::VBO = 0;
+unsigned int Gridbox::EBO = 0;
 
-}
-
-void Gridbox::setup()
+unsigned int Gridbox::indeces[]
 {
-    float vertices[] =
-    {
-        400.0f,300.0f,0.0f,
-        500.0f,300.0f,0.0f,
-        500.0f,200.0f,0.0f,
-        400.0f,200.0f,0.0f
-    };
-
-    unsigned int indeces[]
-    {
         0,1,3,
         2,3,1,
-    };
+};
+
+float Gridbox::vertices[]
+{
+        0.0f,0.0f,0.0f,
+        1.0f,0.0f,0.0f,
+        1.0f,1.0f,0.0f,
+        0.0f,1.0f,0.0f
+};
+
+
+Gridbox::Gridbox()
+{
+    itemID = 0;
+    count = 0;
+    minPos = glm::vec2(0,0);
+    maxPos = glm::vec2(0,0);
+    color =  glm::vec3(0,0,0);
+}
+
+void Gridbox::initialize()
+{
+
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -43,6 +51,14 @@ void Gridbox::setup()
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
+void Gridbox::setup(glm::vec2 min, glm::vec2 max, glm::vec3 _color)
+{
+    itemID = 0;
+    count = 0;
+    minPos = min;
+    maxPos = max;
+    color = _color;
+}
 
 void Gridbox::setItem(unsigned int _itemID, int _count)
 {
@@ -52,9 +68,20 @@ void Gridbox::setItem(unsigned int _itemID, int _count)
 
 void Gridbox::draw(Shader& shader)
 {
-
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(minPos.x,minPos.y, 0));
+    model = glm::scale(model, glm::vec3(maxPos.x - minPos.x, maxPos.y - minPos.y, 1));
+    shader.setMat4("model", model);
     glDisable(GL_DEPTH_TEST);
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glEnable(GL_DEPTH_TEST);
+}
+
+bool Gridbox::hovers(float mouseX, float mouseY)
+{
+    return
+        (
+            mouseX > minPos.x && mouseX < maxPos.x &&
+            mouseY > minPos.y && mouseY < maxPos.y
+        );
 }
