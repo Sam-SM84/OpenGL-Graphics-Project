d6955b4300d7b9d0f99d7f61249b49f636d9db01
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-07-18 12:20:55 +0330

    Update Gridbox.h

--- Src/Gridbox.h
+++ Src/Gridbox.h
@@ -7,15 +7,22 @@ class Gridbox
 public:
 	unsigned int itemID;
 	int count;
-	glm::vec2 position;
+	glm::vec2 maxPos;
+	glm::vec2 minPos;
+	glm::vec3 color;
 
+	Gridbox(glm::vec2 min,glm::vec2 max,glm::vec3 _color);
 	Gridbox();
-	void setup();
+	void setup(glm::vec2 min, glm::vec2 max, glm::vec3 _color);
+	static void initialize();
 	void draw(Shader& shader);
 	void setItem(unsigned int _itemID, int _count);
+	bool hovers(float mouseX, float mouseY);
 
 private:
-	unsigned int VAO, VBO, EBO;
+	static unsigned int VAO, VBO, EBO;
+	static unsigned int indeces[];
+	static float vertices[];
 
 };
 
