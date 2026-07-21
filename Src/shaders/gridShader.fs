9ac08f94a4650b9299cf29f0abe8d772050744ff
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-07-21 11:40:07 +0330

    Update gridShader.fs

--- Src/shaders/gridShader.fs
+++ Src/shaders/gridShader.fs
@@ -3,9 +3,12 @@
 in vec2 TexCoords;
 out vec4 FragColor;
 
-uniform sampler2D texture_diffuse;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = texture(texture_diffuse, TexCoords);
+    vec4 slot = texture(texture1,TexCoords);
+    vec4 item = texture(texture2,TexCoords);
+    FragColor = mix(slot,item,item.a);
 }
