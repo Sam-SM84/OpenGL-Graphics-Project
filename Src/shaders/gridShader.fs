6c00778ac6106e67dbd7882d60e6b456895c7a8c
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-07-22 12:41:03 +0330

    Update gridShader.fs

--- Src/shaders/gridShader.fs
+++ Src/shaders/gridShader.fs
@@ -10,5 +10,7 @@ void main()
 {
     vec4 slot = texture(texture1,TexCoords);
     vec4 item = texture(texture2,TexCoords);
+    if(distance(item.rgb,vec3(1.0)) < 0.1)
+        item.a = 0;
     FragColor = mix(slot,item,item.a);
 }
