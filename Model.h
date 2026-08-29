812822635e2ac0d293cd1be2c483b16a520e8701
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-29 18:31:02 +0330

    Update Model.h

--- Model.h
+++ Model.h
@@ -195,12 +195,6 @@ private:
     }
 };
 
-struct LODmodel
-{
-    Model* far;
-    Model* near;
-};
-
 unsigned int TextureFromFile(const char* path, const string& directory, bool gamma)
 {
     string filename = string(path);
