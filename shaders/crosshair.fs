3718a77a58ac01b01bbb3bb63acf6fcc8dbbdc0d
Sam-SM84 <sam.salahimoghadam@gmail.com>
2026-08-31 13:46:51 +0330

    Update crosshair.fs
    color management

--- shaders/crosshair.fs
+++ shaders/crosshair.fs
@@ -4,11 +4,12 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D texture1;
+uniform vec3 color;
 
 void main()
 {    
     vec4 texColor = texture(texture1, TexCoords);
     if(texColor.r > 0.95 && texColor.g > 0.95 && texColor.b > 0.95)
         discard;
-    FragColor = texColor;
+    FragColor = vec4(color,texColor.a);
 }
